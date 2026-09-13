7e87706f736c6291beb402db19348cdd39ec2b45
Julkar Nain <julkarnain929@gmail.com>
2026-09-13 18:31:03 +0600

    logo find error

--- dev-stack-builder/vite.config.ts
+++ dev-stack-builder/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/assignment_5/',
+  base: '/dev-stack-builder/',
 })
