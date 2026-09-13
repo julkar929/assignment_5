1d5b91563dc086dd6cdbad59f9e029b9836761dc
Julkar Nain <julkarnain929@gmail.com>
2026-09-13 18:37:39 +0600

    m

--- dev-stack-builder/vite.config.ts
+++ dev-stack-builder/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/dev-stack-builder/',
+  base: '/assignment_5/',
 })
